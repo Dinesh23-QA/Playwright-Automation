43caf67d1f11b8754aa75524b7841e1b78678500
Dinesh23-QA <dineshkumarjd04@gmail.com>
2025-11-01 11:57:05 +0530

    updated
    updated

---
+++ playwright.config.js
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './web',               // Folder for test files
+  timeout: 30 * 1000,               
+  retries: 0,                        
+  reporter: 'html',                  
+  use: {
+    headless: false,                  
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 10 * 1000,        
+    ignoreHTTPSErrors: true,         // Ignore HTTPS errors
+    video: 'on-first-retry',        
+    screenshot: 'only-on-failure',  
+  },
+  projects: [
+    { name: 'Chromium', use: { browserName: 'chromium' ,channel: 'chrome'} },
+    
+  ],
+});
---
+++ web/Login.test.ts
@@ -0,0 +1,9 @@
+// tests/example.spec.js
+import { test, expect } from '@playwright/test';
+
+test('verify tewst', async ({ page }) => {
+
+await page.goto("https://www.google.com")
+
+ 
+});
